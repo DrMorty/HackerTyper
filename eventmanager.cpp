ac8d42047da54d1c7cb54eabf855cfd0f7fe2011
DrMorty <31830532+DrMorty@users.noreply.github.com>
2019-02-21 00:15:32 +0300

    Update eventmanager.cpp

--- eventmanager.cpp
+++ eventmanager.cpp
@@ -1,34 +1,7 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include "eventmanager.h"
 
+void HackerTyper::AppStart();
 
-struct ScreenResolution
-{
-    int x;
-    int y;
-
-};
-
-
-class HackerTyper
-{
-private:
-
-    ScreenResolution Resolution;
-    std::string Title;
-    int Edge = 30;
-    float Velocity = 0.05;
-
-public:
-
-    std::string AppText;
-    std::string TmpText;
-    HackerTyper(int ScrResX, int ScrResY, std::string title);
-    void AppRun();
-
-
-
-
-};
-
-#endif 
+#endif
