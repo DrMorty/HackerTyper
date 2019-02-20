b9e7ed32cfbd1a2f26d5e3b7704bedf9901b5986
DrMorty <31830532+DrMorty@users.noreply.github.com>
2019-02-20 23:48:21 +0300

    Update eventhandler.cpp

--- eventhandler.cpp
+++ eventhandler.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+
+
 void Scroll(sf::RenderWindow* window, sf::View* view, int Edge, float Velocity)
 {
 
@@ -36,5 +39,49 @@ void TransformText(int* counter, int* indicator, std::string* AppText, std::stri
 
     fflush(0);
 
+
+}
+
+
+
+void HackerTyper::AppRun()
+{
+    sf::RenderWindow window(sf::VideoMode(Resolution.x, Resolution.y), Title);
+    sf::View view( sf::FloatRect(0,0,1600,900));
+
+
+
+
+    int counter = 0;
+    int indicator = 0;
+
+    while(window.isOpen())
+    {
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+
+            if (event.type == sf::Event::KeyPressed)
+            {
+                TransformText(&counter, &indicator, &AppText, &TmpText);
+            }
+        }
+
+        float Velocity = 0.05;
+        int Edge = 30;
+
+        window.clear();
+        DrawTmpText(&window, "arial.ttf", TmpText);
+        Scroll(&window, &view, 30, 0.05);
+
+
+        window.setView(view);
+
+        window.display();
+
+
+    }
 
 }
