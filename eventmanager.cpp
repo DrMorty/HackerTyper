2400df3243c069d39b489d8a1689b8f333ac124e
DrMorty <31830532+DrMorty@users.noreply.github.com>
2019-02-20 23:52:00 +0300

    Update eventmanager.cpp

--- eventmanager.cpp
+++ eventmanager.cpp
@@ -1,46 +1,34 @@
-#include "eventmanager.h"
+#include <iostream>
 #include <SFML/Graphics.hpp>
-#include "eventhandler.h"
 
 
-void HackerTyper::AppRun()
+struct ScreenResolution
 {
-    sf::RenderWindow window(sf::VideoMode(Resolution.x, Resolution.y), Title);
-    sf::View view( sf::FloatRect(0,0,1600,900));
+    int x;
+    int y;
+
+};
+
+
+class HackerTyper
+{
+private:
+
+    ScreenResolution Resolution;
+    std::string Title;
+    int Edge = 30;
+    float Velocity = 0.05;
+
+public:
+
+    std::string AppText;
+    std::string TmpText;
+    HackerTyper(int ScrResX, int ScrResY, std::string title);
+    void AppRun();
 
 
 
 
-    int counter = 0;
-    int indicator = 0;
+};
 
-    while(window.isOpen())
-    {
-        sf::Event event;
-        while(window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-
-            if (event.type == sf::Event::KeyPressed)
-            {
-                TransformText(&counter, &indicator, &AppText, &TmpText);
-            }
-        }
-
-        float Velocity = 0.05;
-        int Edge = 30;
-
-        window.clear();
-        DrawTmpText(&window, "arial.ttf", TmpText);
-        Scroll(&window, &view, 30, 0.05);
-
-
-        window.setView(view);
-
-        window.display();
-
-
-    }
-
-}
+#endif 
